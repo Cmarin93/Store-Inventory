284bd61d095349f8c37ee991556eeb972dd7a32a
Cmarin93 <45906528+Cmarin93@users.noreply.github.com>
2020-02-15 18:52:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import csv
 
-with open('inventory.csv', newline='') as csvfile:
-     inventory = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     for row in inventory:
-         print(', '.join(row))
+with open('inventory.csv', 'r', newline='') as csvfile:
+     inventory = csv.reader(csvfile)
+     next(inventory)
+     for line in inventory:
+         print(line[0])

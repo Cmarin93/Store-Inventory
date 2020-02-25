b0532c48b398d0a0ac1ba3fe52856bbd48ac348a
Cmarin93 <45906528+Cmarin93@users.noreply.github.com>
2020-02-25 08:55:04 -0800

    isitworking

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+#!/usr/bin/env python3
+import datetime
 import csv
+from peewee import *
+
+
+db = SqliteDatabase('stock.db')
 
 with open('inventory.csv', 'r', newline='') as csvfile:
      inventory = csv.reader(csvfile)
-     next(inventory)
-     for line in inventory:
-         print(line[0])
+     next(inventory) # skips first line.
+     for item in inventory:
+         print(item[0])
+
+
+class Product(Model):
+    id = AutoField(unique=True)
+    name = CharField(max_length=255, unique=True)
+    price = IntegerField()
+    quantity = IntegerField()
+    last_checked = DateTimeField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Product], safe=True)

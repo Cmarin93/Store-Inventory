d41e6ec2b6c35b613297a9179113f8bc791d5e00
Cmarin93 <c.marin93.64@gmail.com>
2020-03-29 23:35:21 -0700

    2-29-20 | Updated add_products()

--- app.py
+++ app.py
@@ -6,59 +6,92 @@ from peewee import *
 
 db = SqliteDatabase('inventory.db')
 
-# with open('inventory.csv', 'r', newline='') as csvfile:
-#      inventory = csv.reader(csvfile)
-#      next(inventory) # skips first line.
-#      for item in inventory:
-#          print(item[0])
 
+def fetch_products_from_csv():
+    ''' convert csv to a list of ordered dictionaries'''
+    with open('inventory.csv', newline='') as csvfile:
+        products = list(csv.DictReader(csvfile)) 
+    return products
 
 class Product(Model):  
     #product_id = AutoField(unique=True)
     product_name = CharField(max_length=75, unique=True)
-    product_price = IntegerField()
-    #product_quantity = IntegerField(default=0)
-    #date_updated = DateTimeField(datetime.datetime.now())
+    product_price = IntegerField(default=0)
+    product_quantity = IntegerField(default=0)
+    #date_updated = DateField()
 
     class Meta: #telling the model which database it belongs to.
         database = db
 
 
-
-products = [
-    {
-    'name': 'apple',
-    'price': '$1.29'},
-    {
-    'name': 'banana',
-    'price': '$1.98'},
-    {
-    'name': 'carrot',
-    'price': '$2.79'}
-] 
-
 def add_products():
+    products = fetch_products_from_csv()
     for product in products:
+        product_record = Product.get_or_create(product_name=product.get('product_name'))
         try:
-            Product.create(product_name=product['name'], product_price=product['price'])
-        except IntegrityError:
-            product_record = Product.get(product_name=product['name']) #gets product out of db
-            if product_record.product_price != product['price']:
-                print(f"The price of {product_record.product_name} has changed from {product_record.product_price} to {product['price']}.")
-                product_record.product_price = product['price'] # set new price
-            product_record.save()
+            quantity = int(product.get('product_quantity'))
+        except TypeError:
+            quantity = 0
+        product_record[0].product_quantity = quantity
+        try:
+            price = product.get('product_price')
+        except TypeError:
+            price = 0
+        product_record[0].product_price = price
+        print(product_record)
+        product_record[0].save()
+        if not product_record[1]: # if product was not added to db
+            print("WAS NOT ADDED")
 
 
-def cheapest_product():
-    cheap = Product.select().order_by(Product.product_price.asc()).get()
-    return cheap.product_name
+
+        # save()
+        # try:
+        #     # price = cleaned up price value
+        # except:
+        #     # price = default price value
+       
 
 if __name__ == '__main__':
     db.connect()
     db.create_tables([Product], safe=True)
     add_products()
-    print(f"The cheapest product is: {cheapest_product()}")
     db.close()
 
 
-    #  3/25/2020 - carlos a marin
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #  3/27/2020 - carlos a marin
+
+
+
+
+
+            #if quantity is not None:
+            #     quantity = int(quantity)
+            # else: 
+            #     quantity = 0
+
+
+
+
+            #         # 
+            # product_record = Product.get(product_name=product['name']) #gets product out of db
+            # if product_record.product_price != product['price']:
+            #     print(f"The price of {product_record.product_name} has changed from {product_record.product_price} to {product['price']}.")
+            #     product_record.product_price = product['price'] # set new price
+            # product_record.save()
+
+
+

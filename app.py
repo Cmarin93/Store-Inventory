d81fd3c1110afca75d2c8f8dfdd7cd621dcdd8c4
Cmarin93 <c.marin93.64@gmail.com>
2020-04-12 19:17:30 -0700

    structure changes

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def fetch_products_from_csv():
 
 
 # ERROR WITH LOGIC:  1. The date needs to determine what data shall be saved into the DB.
-#             Hypo:  2. When looping thru entries - the rest of entires data overides the records data.
+#
+#        What if there are 2 entries entered within the same date?
 
 class Product(Model):  
     id = AutoField(unique=True)
@@ -29,79 +30,73 @@ class Product(Model):
     class Meta: #telling the model which database it belongs to.
         database = db
 
+# CURRENTLY: researching how to skip an iteration of an entry that has been rejected.
+def verify_product_data(entry):
+    validate_date(entry)
+    validate_price(entry)
+    validate_quantity(entry)
+
+
+def validate_date(entry):
+    try:
+        isValidDate = bool(re.match(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", entry['date_updated']))
+        if isValidDate:
+            print(f"{entry['name']} | {entry['date_updated']}") # NAME | DATE
+        else:
+            raise ValueError()
+    except ValueError:
+        input(f"{entry['name']}: Invalid date.")
+        breakpoint()
+
+def validate_price(entry):
+    try:
+        isValidPrice = bool(re.match(r"^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$", entry['price']))
+        if isValidPrice:
+            print(f"Price: {entry['price']}")
+        else:
+            raise TypeError()
+    except TypeError:
+        input(f"{entry['name']}: Invalid price")
+        #continue
+
+def validate_quantity(entry):
+    try:
+        int(entry['quantity'])
+        print(f"Qty: {entry['quantity']}")
+        input("")
+    except TypeError:
+        print(f"{entry['name']}: Invalid quantity.")
+        input("")
+        #continue
+
+
+# WRITE: CONVERT_DATA or Implement data into validation.
+
+
+
 
 def add_products():
     """Creates or updates every product"""
     product_enteries = fetch_products_from_csv()
     for entry in product_enteries:
-        product_record = Product.get_or_create(name=entry.get('name'))
-
-# verifying product_price
-        try:
-            print('')
-            valid_price_entry = re.match(r"^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$", entry['price'])
-            if valid_price_entry:
-                digit_regex = re.compile(r"[^\d]+")
-                price = digit_regex.sub("", entry['price'])
-            else:
-                raise TypeError()
-        except TypeError:
-            price = 0
-        product_record[0].price = price
-# verifying product_quantity
-        try:
-            quantity = int(entry['quantity'])
-        except TypeError:
-            quantity = 0
-        product_record[0].quantity = quantity
- # verifying date_update
-        try:
-            finalDate = product_record[0].date_updated
-            isValidDate = bool(re.match(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", entry['date_updated'])) 
-            if isValidDate:
-                isNewProduct = product_record[1]
-                if isNewProduct:
-                    finalDate = datetime.strptime(entry['date_updated'], '%m/%d/%Y').date()
-                    print(f"{entry['name']} has been added to the database!")
-                else:
-                    record_date = product_record[0].date_updated
-                    entry_date = datetime.strptime(entry['date_updated'], '%m/%d/%Y').date()
-                    finalDate = greater_then_date(record_date, entry_date)
-                    print(f"{entry['name']} has been updated!")
-            else:
-                raise TypeError()
-        except TypeError as e:
-            date_object = 0
-        product_record[0].date_updated = finalDate
-        product_record[0].save()
-    print('')
-
-
-def verify_date(record, entry):
-    pass
-
-
-def greater_then_date(record, entry):
-    if record > entry:
-        greater_date = record
-    else:
-        greater_date = entry
-    return greater_date
+        verify_product_data(entry)
+        # I do not understand this function completely.
+        product_record = Product.get_or_create(name=entry['name'], date_updated=entry['date_updated']) # mapping of field names to value.
 
 
 if __name__ == '__main__':
     db.connect()
-    db.create_tables([Product], safe=True)
+    db.create_tables([Product], safe=True) # Structure of table is set-up, but no data is inserted.
     add_products()
     db.close()
 
 
 
+                    # PRICE CONVERTION
+            # digit_dissection = re.compile(r"[^\d]+")
+            # price_reformed = digit_dissection.sub("", entry['price'])
+            # entry['price'] = price_reformed
 
 
-# 'valid_price_entry' regex by "Brian Orrell": http://regexlib.com/UserPatterns.aspx?authorId=f77b664d-b24a-4461-8e5f-8ea36aa47f58 (not secure connection)
-
-# notes about 'valid price entry':
-# '$0.00' = '000'
 
 

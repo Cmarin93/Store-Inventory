9fd3fcdd0bae88c621e453052ba1db934819a10a
Cmarin93 <c.marin93.64@gmail.com>
2020-04-19 11:02:39 -0700

    Entry validation/conversion is working; now to refactor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+8#!/usr/bin/env python3
 from datetime import datetime
+from dateutil.parser import parse
 import csv
 import re
 from peewee import *
@@ -18,11 +19,11 @@ def fetch_products_from_csv():
 
 # ERROR WITH LOGIC:  1. The date needs to determine what data shall be saved into the DB.
 #
-#        What if there are 2 entries entered within the same date?
+#        What if there are 2 identital product entries entered within the same date? (add the later entry)
 
 class Product(Model):  
     id = AutoField(unique=True)
-    name = CharField(max_length=75)
+    name = CharField(max_length=75, unique=True)
     price = IntegerField(default=0) 
     quantity = IntegerField(default=0)
     date_updated = DateField(default=0) # replace default w/ datetime.date object?
@@ -31,25 +32,41 @@ class Product(Model):
         database = db
 
 
-def convert_price(entry):
-    digit_dissection = re.compile(r"[^\d]+")
-    price_reformed = digit_dissection.sub("", entry['price'])
-    entry['price'] = price_reformed
-
 def verify_product_data(entry):
     print("")
+    validate_date(entry)
     validate_price(entry)
     validate_quantity(entry)
-    validate_date(entry)
     print("")
-# DESIGN QUESTION: Should I convert fields within the validation on the entry?
+
+
+def validate_date(entry):
+    """Validates entries date"""
+    isValidDate = bool(re.match(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", entry['date_updated']))
+    if isValidDate:
+        datetime_object = parse(entry['date_updated'])
+        date_object = datetime_object.date()   
+        entry['date_updated'] = date_object
+    else:
+        print("Invalid date.")
+        raise ValueError()
+
+# Price
 
 def validate_price(entry):
     isValidPrice = bool(re.match(r"^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$", entry['price']))
     if isValidPrice:
         convert_price(entry)
     else:
+        print("Invalid Price.")
         raise TypeError()
+
+def convert_price(entry):
+    digit_dissection = re.compile(r"[^\d]+") 
+    price_reformed = digit_dissection.sub("", entry['price']) 
+    entry['price'] = price_reformed
+
+# Quantity
 
 def validate_quantity(entry):
     try:
@@ -58,39 +75,17 @@ def validate_quantity(entry):
         print("Invalid quantity")
         raise TypeError()
 
-def validate_date(entry):
-    """Validates entries date"""
-    isValidDate = bool(re.match(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", entry['date_updated']))
-    if isValidDate:
-        #convert entry['date_updated'] to a datetime.date object.
-        print(f"{entry['name']} was entered into the system on {entry['date_updated']}") 
-    else:
-        raise ValueError()
-
-
 def compare_entry_with_records():
     pass
 
 
 def import_products():
     """Creates or updates every product"""
-    productEntries = fetch_products_from_csv()
-    for entry in productEntries:
+    entries = fetch_products_from_csv()
+    for entry in entries:
         try:
-            verify_product_data(entry)
-            #
-            # attempt to get record w/ same name + date.
-            # if found: rewrite, price + quantity. 
-            # if not found: create new record.
-            breakpoint()
-
-            # type(entry['date_updated']) = <class 'str'>   
-
-            # This format imports only name (all other values = 0)
-            product_record = Product.get_or_create(name=entry['name']) # mapping of field names to value.
-            # This format will place dulicates.
-            product_record2 = Product.get_or_create(name=entry['name'], price=entry['price'], quantity=entry['quantity'], date_updated=entry['date_updated'])
-
+            verify_product_data(entry) # entries data is verified in : correct_format
+            #breakpoint()
         except:
             print(f"{entry['name']}: Invalid entry.")
             continue
@@ -101,4 +96,3 @@ if __name__ == '__main__':
     db.create_tables([Product], safe=True) # Structure of table is set-up, but no data is inserted.
     import_products()
     db.close()
-
